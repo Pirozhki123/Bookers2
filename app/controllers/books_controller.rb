c4f76b8289c38eb45f28aba8f4a739d87396c93f
Pirozhki <Pirozhki123@users.noreply.github.com>
2023-01-07 08:26:45 +0000

    [add]bookのcreate機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,7 @@ class BooksController < ApplicationController
   def index
     @user = current_user
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -12,6 +13,18 @@ class BooksController < ApplicationController
 
   def new
     @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books'
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 
 end
