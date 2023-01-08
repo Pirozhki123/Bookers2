f4743813cc62fe7b17ac1301e7e33117e33979e1
Pirozhki <Pirozhki123@users.noreply.github.com>
2023-01-08 06:28:57 +0000

    [add]bookのindexページの右側の作成(Books)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,11 +12,11 @@ class BooksController < ApplicationController
   end
 
   def new
-    @book = Book.new
   end
 
   def create
     book = Book.new(book_params)
+    book.user_id = current_user.id
     book.save
     redirect_to '/books'
   end
