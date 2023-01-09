99b446a3a86a831f3c15ddc91bc934643da0829d
Pirozhki <Pirozhki123@users.noreply.github.com>
2023-01-09 04:59:11 +0000

    [add]userのeditページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,23 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
   end
 
   def edit
+    @user = current_user
   end
 
   def index
     @user = current_user
     @users = User.all
     @book = Book.new
+  end
+
+  def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to user_path(current_user)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'homes/about' => 'homes#about'
   devise_for :users
   resources :books, only: [:edit, :create, :index, :show, :destroy, :update]
-  resources :users, only: [:index, :edit, :show]
+  resources :users, only: [:index, :edit, :show, :update, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
